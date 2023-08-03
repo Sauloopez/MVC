c4cd18d607ae59c6d1c54c13da1d7e20947eb611
Sauloopez <sauleta.selb@gmail.com>
2023-08-03 09:24:33 -0500

    Generando vistas de prueba

---
+++ App/Views/copias/nuevacopia.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nueva Copia</title>
+</head>
+<body>
+    <p>Aqui es pa' crear una nueva copia</p>
+</body>
+</html>
