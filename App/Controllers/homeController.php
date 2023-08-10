a8117704bdec69171b7da3fc01431e302309f876
Sauloopez <sauleta.selb@gmail.com>
2023-08-10 13:24:46 -0500

    Establecido el formato de método inicial de controlador
    Se establece como el método default para el controlador, index

--- App/Controllers/homeController.php
+++ App/Controllers/homeController.php
@@ -4,6 +4,9 @@ class homeController extends ControlBase{
     private $dir;
     public function __construct(){
         parent::__construct();
+    }
+
+    public function index(){
         $this->view->show('home/index');
     }
 }
--- App/Modules/Router.php
+++ App/Modules/Router.php
@@ -11,31 +11,32 @@ class Router
 
     public function matchRoute()
     {
-        $query = explode('=', $_SERVER['QUERY_STRING'])[1];
-        $url = explode('/', $query);
-        $dirController = __DIR__ . '/../Controllers/' . $url[0] . 'Controller.php';
+        $query = isset(explode('=', $_SERVER['QUERY_STRING'])[1]) ? explode('=', $_SERVER['QUERY_STRING'])[1] : 'home';
+
+        define('url', explode('/', $query));
+        $dirController = __DIR__ . '/../Controllers/' . url[0] . 'Controller.php';
         if (is_file($dirController)) {
             require_once($dirController);
 
-            $this->controller = $url[0].'Controller';
-            $this->method = '__construct';
-            if(isset($url[1])){
-                $this->method = $url[1];
+            $this->controller = url[0] . 'Controller';
+            $this->method = 'index';
+            if (isset(url[1])) {
+                $this->method = url[1];
             }
             $this->run();
-        }else{
+        } else {
             ControlBase::Error();
         }
     }
 
-    private function run(){
+    private function run()
+    {
         $controller = new $this->controller;
-        
-        if($this->method != '__construct'){
-            if(method_exists($controller, $this->method))
-                $controller ->{$this->method}();
-            else
-                ControlBase::Error();
-        }
+
+        if (method_exists($controller, $this->method))
+            $controller->{$this->method}();
+        else
+            ControlBase::Error();
+
     }
 }
